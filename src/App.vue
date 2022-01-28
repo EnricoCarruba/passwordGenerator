1b50ec0f47d8ce4461ae60a208aec7311a24189f
Enrico Carruba <carrubaenrico@gmail.com>
2022-01-28 15:50:54 +0100

    imported registred and used passwordgen. template

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <passwordgenerator />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import passwordgenerator from "./components/password-generator.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    passwordgenerator,
+  },
+};
 </script>
 
 <style>
